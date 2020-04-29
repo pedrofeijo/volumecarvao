228e8a022f9785cd2bcf4df79cebbc5c17da2886
AdrianoI15U <adrianordp@alu.ufc.br>
2020-04-29 12:36:42 -0300

    Commenting code and applying status messages

--- pcselector.py
+++ pcselector.py
@@ -12,12 +12,21 @@ from   PyQt5 import QtWidgets, QtGui, QtCore
 from   Xlib.display import Display
 from   PIL import Image
 
+# GLOBAL VARIABLES
+# ID of pptk window for embeding procedure
 winId = 0
+# Path to main directory
 root  = os.path.dirname(os.path.abspath(__file__)) + '/'
-pathToSlices = root + '.selecao_teste'
+# Path to temporary folder
+pathToSlices = root + '.selecao_teste/'
+# Path to cached point cloud
+pathToCachedPC = root + '.selected.txt'
+# Flag to detect changes of point cloud
 flagModification = False
 
-def findViewer(window, indent): # Search for a window called "viewer"
+# Search for a window called "viewer"
+def findViewer(window, indent):
+    # Modified global variables
     global winId
     children = window.query_tree().children
     for w in children:
@@ -27,7 +36,9 @@ def findViewer(window, indent): # Search for a window called "viewer"
                 # Save "viewer" window ID
                 winId = w.id
 
-def getPC(): # Display a dummy point cloud
+# Display a dummy point cloud to start the interface
+def getPC():
+    # Changing global variables
     global v
 
     # Dummy point cloud
@@ -35,19 +46,20 @@ def getPC(): # Display a dummy point cloud
     v.set(bg_color=[1.0,1.0,1.0,0.0])
     v.set(floor_color=[1.0,1.0,1.0,0.0])
 
+# Main window code
 class MainWindow(QtWidgets.QMainWindow):
+    # Initialization function
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        # Widget object
         self.mywidget = QtWidgets.QWidget()
+        # Layout object
         self.mylayout = QtWidgets.QGridLayout(self.mywidget)
         self.setCentralWidget(self.mywidget)
 
-        getPC()
-        self.xlib = Display().screen().root
-        findViewer(self.xlib, '-')
-        self.window = QtGui.QWindow.fromWinId(winId)
-        self.windowcontainer = self.createWindowContainer(self.window, self.mywidget)
+        
+        # Creating button objects
         self.buttonLoad    = QtWidgets.QPushButton("Carregar nuvem")
         self.buttonConfirm = QtWidgets.QPushButton("Confirmar seleção")
         self.buttonVolume  = QtWidgets.QPushButton("Calcular volume")
@@ -55,8 +67,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.buttonUndo    = QtWidgets.QPushButton("Desfazer última seleção")
         self.buttonRedo    = QtWidgets.QPushButton("Refazer seleção")
         self.buttonClose   = QtWidgets.QPushButton("Fechar")
-        self.dialogBox = QtWidgets.QTextEdit("Área de informações")
+
+        # Disabling buttons for latter usage
+        self.buttonVolume.setEnabled(False)
+        self.buttonConfirm.setEnabled(False)
+        self.buttonSave.setEnabled(False)
+        self.buttonUndo.setEnabled(False)
+        self.buttonRedo.setEnabled(False)
         
+        # Defining button functions
         self.buttonLoad.clicked.connect(self.loadClick)
         self.buttonConfirm.clicked.connect(self.confirmClick)
         self.buttonVolume.clicked.connect(self.calcClick)
@@ -65,7 +84,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.buttonRedo.clicked.connect(self.redoClick)
         self.buttonClose.clicked.connect(self.closeClick)
 
-        self.mylayout.addWidget(self.windowcontainer, 0, 1, 8, 10)
+        # Creating a dialog box object
+        self.dialogBox = QtWidgets.QTextEdit("Área de informações")
+        self.dialogBox.setReadOnly(True)
+
+        # Layout setup (except pptk container)
+        self.mylayout.setColumnStretch(1, 3)
         self.mylayout.addWidget(self.buttonLoad     , 0, 0)
         self.mylayout.addWidget(self.buttonConfirm  , 1, 0)
         self.mylayout.addWidget(self.buttonVolume   , 2, 0)
@@ -74,28 +98,54 @@ class MainWindow(QtWidgets.QMainWindow):
         self.mylayout.addWidget(self.buttonRedo     , 5, 0)
         self.mylayout.addWidget(self.buttonClose    , 6, 0)
         self.mylayout.addWidget(self.dialogBox      , 7, 0)
-        self.mylayout.setColumnStretch(1, 3)
+        
+        # Creating a dummy pptk window
+        getPC()
+        self.embedPC()
+    
+    def embedPC(self):
+        # Find pptk window ID
+        self.xlib = Display().screen().root
+        findViewer(self.xlib, '-')
+        # Creating a window object
+        self.window = QtGui.QWindow.fromWinId(winId)
+        # Defining container object
+        self.windowcontainer = self.createWindowContainer(self.window, self.mywidget)
+        # Setting container to layout
+        self.mylayout.addWidget(self.windowcontainer, 0, 1, 8, 10)
 
-        self.buttonVolume.setEnabled(False)
-        self.buttonConfirm.setEnabled(False)
-        self.buttonSave.setEnabled(False)
-        self.buttonUndo.setEnabled(False)
-        self.buttonRedo.setEnabled(False)
-
+    # Load new point cloud
     def loadClick(self):
-        global v, xyz, root, pathToSlices
-        fname = QtWidgets.QFileDialog.getOpenFileName(self, "Escolher nuvem de pontos", root, "Arquivos de nuvem de pontos (*.txt)")
-        if fname ==('',''):
-            return
-        nuvem = fname[0]
+        # Modified global variables
+        global v, xyz
 
+        # Status message
+        self.dialogBox.clear()
+        self.dialogBox.textCursor().insertText('Escolhendo nuvem de pontos...\n')
+
+        # Open a dialog box
+        fname = QtWidgets.QFileDialog.getOpenFileName(self, "Escolher nuvem de pontos", root, "Arquivos de nuvem de pontos (*.txt)")
+        # If nothing is selected: return
+        if fname ==('',''):
+            self.dialogBox.clear()
+            self.dialogBox.textCursor().insertText('Nenhuma nuvem escolhida!\n')
+            return
+        # Get file name
+        nuvem = fname[0]
+        # Status message
+        self.dialogBox.clear()
+        self.dialogBox.textCursor().insertText('Arquivo: ' + nuvem + '.')
+        # Try to load the txt point cloud into a numpy float matrix.
         try:
-            # Try to load the txt point cloud into a numpy float matrix
             xyz = np.loadtxt(nuvem, delimiter= ' ')
         except:
-            # Display error message if load fails
-            sys.exit('Error loading ' + nuvem + '!')
+            self.dialogBox.textCursor().insertText('\nErro: Arquivo inválido!\n')
+            getPC()
+            self.embedPC()
+            return
 
+        # Status message
+        self.dialogBox.textCursor().insertText(' Ok!')
         # Filter x, y and z coordinates
         xyz = xyz[:,:3]
         # Register z values (used to coloring)
@@ -107,55 +157,64 @@ class MainWindow(QtWidgets.QMainWindow):
         M2 = bxplt['whiskers'][1]._y[1] # Maximum value of the maximum range
         # plt.show() # displays boxplot
 
-        # Load point cloud to viewer referencing z axis to colors
+        # Load point cloud to pptk viewer referencing z axis to colors
         v = pptk.viewer(xyz,z)
-        # Displays point cloud
         v.color_map('jet',scale=[m1,M2])
         v.set(bg_color = [1.0,1.0,1.0,0.0])
         v.set(floor_color = [1.0,1.0,1.0,0.0])
-        self.xlib = Display().screen().root
-        findViewer(self.xlib, '-')
-        self.window = QtGui.QWindow.fromWinId(winId)
-        self.windowcontainer = self.createWindowContainer(self.window, self.mywidget)
-        self.mylayout.addWidget(self.windowcontainer, 0, 1, 8, 10)
+        # Embed pptk
+        self.embedPC()
         self.buttonConfirm.setEnabled(True)
 
+    # Save current point cloud
     def saveClick(self):
+        # Modified global variables
         global flagModification
-        self.dialogBox.moveCursor(QtGui.QTextCursor.End)
+        
+        self.dialogBox.textCursor().insertText('Salvando nuvem de pontos...')
         fname = QtWidgets.QFileDialog.getSaveFileName(self, 'Salvar nuvem de pontos', root, "Arquivos de nuvem de pontos (*.txt)")
         if fname == ('',''):
+            self.dialogBox.textCursor().insertText(' Cancelar!')
             return
         file = open(fname[0],'w')
-        text = open(root+'.selected.txt','r').read()
+        text = open(pathToCachedPC,'r').read()
         file.write(text)
         file.close()
+        self.dialogBox.textCursor().insertText('Ok!\n')
         self.dialogBox.textCursor().insertText('Nuvem de pontos salva em: '+fname[0]+'\n')
         flagModification = False
 
+    # Return to previous modification state
     def undoClick(self):
         self.dialogBox.moveCursor(QtGui.QTextCursor.End)
         self.dialogBox.textCursor().insertText('Undo')
         self.buttonRedo.setEnabled(True)
 
+    # Return to later modification state after Undo
     def redoClick(self):
         self.dialogBox.moveCursor(QtGui.QTextCursor.End)
         self.dialogBox.textCursor().insertText('Redo')
 
+    # Close application
     def closeClick(self):
         self.close()
         
+    # Confirm modification
     def confirmClick(self):
-        global xyz, v, root, pathToSlices, flagModification
+        # Modified global variables
+        global xyz, v, root, flagModification
+        
+        # Status message
         self.dialogBox.clear()
         self.dialogBox.textCursor().insertText('Buscando ponto selecionados... ')
 
-        ##### Segmentar Nuvem de Pontos
+        ##### Segmentar Nuvem de Pontos #####
         # Collects selected points indexes
         sel = v.get('selected')
         len(sel)
         # Create a numpy matrixes of selected points
-        if len(sel)==0:
+        if len(sel) == 0:
+            # Status message
             self.dialogBox.moveCursor(QtGui.QTextCursor.End)
             self.dialogBox.textCursor().insertText('Nenhum ponto selecionado!\nUtilize o botão esquerdo do mouse (BEM) com a tecla Control para efetuar seleção no campo de nuvem de pontos: BEM+Ctrl')
             return
@@ -163,8 +222,9 @@ class MainWindow(QtWidgets.QMainWindow):
             self.dialogBox.textCursor().insertText('Ok!\n')
             # Path to slices
             try:
-                # Tries to make the directory ".selecao_teste"
+                # Tries to make temporary directory
                 os.mkdir(pathToSlices)
+                # Status message
                 self.dialogBox.textCursor().insertText('Criando diretório temporário... Ok!\n')
             except:
                 pass
@@ -183,14 +243,10 @@ class MainWindow(QtWidgets.QMainWindow):
         v.color_map('jet',scale=[m1,M2])
         v.set(bg_color=[1.0,1.0,1.0,0.0])
         v.set(floor_color=[1.0,1.0,1.0,0.0])
-        self.xlib = Display().screen().root
-        findViewer(self.xlib, '-')
-        self.window = QtGui.QWindow.fromWinId(winId)
-        self.windowcontainer = self.createWindowContainer(self.window, self.mywidget)
-        self.mylayout.addWidget(self.windowcontainer, 0, 1, 8, 10)
+        self.embedPC()
         
         # Save archive with selected points
-        np.savetxt('.selected.txt', xyz) # Transposta dos dados
+        np.savetxt(pathToCachedPC, xyz) # Transposta dos dados
 
         flagModification = True
         self.buttonVolume.setEnabled(True)
@@ -203,10 +259,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.dialogBox.textCursor().insertText('Calculando...\n')
         # LER NUVEM DE PONTOS
         # Set root path to selected points
-        arquivo  = root + ".selected.txt"
         try:
             # Try to load the txt point cloud into a numpy float matrix
-            dados_df = np.loadtxt(arquivo, delimiter= ' ')
+            dados_df = np.loadtxt(pathToCachedPC, delimiter= ' ')
         except:
             dados_df = xyz
         dados_df = dados_df[:,:3] # ajustar arquivo txt - (linha , coluna)
@@ -238,11 +293,11 @@ class MainWindow(QtWidgets.QMainWindow):
             plt.axis("off") # sem eixos
             
             # Plotar arquivo .txt de cada slice
-            fig.savefig(root+'.selecao_teste/fig_{}.png'.format(i))
+            fig.savefig(pathToSlices+'/fig_{}.png'.format(i))
             print(i) 
 
             points_slice = [(x,y,z) for x,y,z in zip(dados_x[i*intervalo: (i+1)*intervalo],dados_y[i*intervalo: (i+1)*intervalo],dados_z[i*intervalo: (i+1)*intervalo])]
-            np.savetxt(root+'.selecao_teste/points_fig_{}.txt'.format(i), points_slice, delimiter=' ') 
+            np.savetxt(pathToSlices+'/points_fig_{}.txt'.format(i), points_slice, delimiter=' ') 
 
             plt.close()
 
@@ -253,7 +308,7 @@ class MainWindow(QtWidgets.QMainWindow):
         total = 0
 
         for i in range(len(filepaths)):
-            img = np.asarray(Image.open(root + ".selecao_teste/fig_{}.png".format(i)).convert('L'))
+            img = np.asarray(Image.open(pathToSlices + "fig_{}.png".format(i)).convert('L'))
             img = 1 * (img < 255)
             m,n = img.shape
             total += img.sum() 
